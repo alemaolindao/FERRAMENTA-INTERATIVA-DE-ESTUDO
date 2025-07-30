00af6eb146c6abdd328679db21724cc17cfccde0
murilo <pereira.costa.murilo@escola.pr.gov.br>
2025-07-30 07:32:26 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Carros preparados',
+    'Qual a melhor turbina para motor AP?',
+    'turbina auto avionics A40-2B.'
+)
+
+criaCartao(
+    'Carros preparados',
+    'Qual o melhor pistão e biela de motor AP?',
+    'São os pistoes AFP e SPA.'
+)
+
+criaCartao(
+    'Carros preparados',
+    'Qual o maximo de potência que posso extrair de um motor AP?',
+    'O maximo de potência que pode ser extraido de um motor AP é de no maximo 2000 cavalos.'
+)
