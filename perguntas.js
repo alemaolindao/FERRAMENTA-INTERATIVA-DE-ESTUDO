159fb46de34c17587ce49255fef07a7878667542
murilo <pereira.costa.murilo@escola.pr.gov.br>
2025-08-20 07:22:57 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -15,3 +15,8 @@ criaCartao(
     'Qual o maximo de potência que posso extrair de um motor AP?',
     'O maximo de potência que pode ser extraido de um motor AP é de no maximo 2000 cavalos.'
 )
+
+criaCartao(
+    'Carros preparados',
+    'existe algum campeão de armageddon usando um gol?',
+    'sim, a 13ª edição do armageddon teve um campeão usando um gol ap 16v.'
